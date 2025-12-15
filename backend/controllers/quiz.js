e523cd3d3e37ad53e4c621aa2d6714aa6ea7e0b9
Ramona Menezes <ramonaM26>
2025-12-15 22:50:28 +0530

    Created Controller for Quizes

---
+++ backend/controllers/quiz.js
@@ -0,0 +1,57 @@
+const Quiz = require('../models/Quiz');
+const quizModel = require('../models/Quiz');
+
+const getAllQuizzes = async (req, res) => {
+
+    try{
+        const questions = await Quiz.getAllQuestionsAndAnswers();
+        res.status(200).json(questions);
+    } catch (error) {
+        res.status(500).json({message : "Error Fetching Quizzes", error : error.message});
+    };
+};
+
+const getQuizById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const questions = await Quiz.getOneQuestion(id);
+
+        if (!questions){
+            return res.status(404).json({message:"Question not found"});
+        }
+        res.status(200).json(questions);
+
+    } catch (error) {
+        res.status(500).json({message:"Error Fetching Question", error:error.message});
+    }
+};
+
+
+const removeQuestion = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Quiz.deleteQuestion(id);
+        res.status(200).json({message:"Question Deleted"});
+
+    } catch (error) {
+        res.status(500).json({message:"Error Deleting Question", error:error.message});
+    }
+};
+
+const createQuestion = async (req, res) => {
+    try {
+        const newQuestion = await Quiz.addQuestion(req.body);
+        res.status(201).json({message:"Question added", data:newQuestion});
+    } catch(error) {
+        res.status(400).json({message:'Failed to add question', error:error.message})
+    }
+};
+
+module.exports = {
+    getAllQuizzes,
+    getQuizById,
+    removeQuestion,
+    createQuestion
+    
+};
+    
