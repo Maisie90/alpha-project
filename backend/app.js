8c188d91ab2f2bb9f02dd860c03c39983593748e
Ramona Menezes <ramonaM26>
2025-12-15 21:49:36 +0530

    Worked on  Creating App and Index JS files and added env

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//Middleware to parese JSON Bodies (Handling data from frontend)
+app.use(express.json());
+app.use(cors());
+
+//check if backend is working
+app.get('/',(req, res) => {
+    res.status('200').send('Server is Running');
+
+});
+
+module.exports = app;
