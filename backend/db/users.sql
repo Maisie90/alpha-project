4f5d0e56abdc85f267d1064537bb798759229eed
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-15 19:20:07 +0000

    update user_role enum to be 'student' and 'teacher'

--- backend/db/users.sql
+++ backend/db/users.sql
@@ -1,10 +1,10 @@
-CREATE TYPE "public"."user_role" AS ENUM('user', 'admin');--> statement-breakpoint
+CREATE TYPE "public"."user_role" AS ENUM('student', 'teacher');--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"username" text NOT NULL UNIQUE,
 	"password" text NOT NULL,
 	"name" text NOT NULL,
-	"role" "user_role" DEFAULT 'user' NOT NULL,
+	"role" "user_role" NOT NULL,
 	"imageUrl" text,
 	"deletedAt" timestamp with time zone,
 	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
