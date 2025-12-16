392472fe4811c555ba7227766594077b67896032
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-16 13:12:33 +0000

    wrote first test

--- __tests__/user/integration/userEndpoints.spec.js
+++ __tests__/user/integration/userEndpoints.spec.js
@@ -1,4 +1,32 @@
-const request = require('supertest');
+const request = require("supertest")
 const app = require("../../../backend/app")
-const { resetTestDB } = require("./config");
+const { resetTestDB } = require("./config")
 
+describe("User API Endpoints", () => {
+  let api
+
+  beforeEach(async () => {
+    await resetTestDB()
+  })
+
+  beforeAll(() => {
+    api = app.listen(4000, () => {
+      console.log("Test server running on port 4000")
+    })
+  })
+
+  afterAll((done) => {
+    console.log("Gracefully closing server")
+    api.close(done)
+  })
+
+  describe("GET /", () => {
+    it("responds to GET / with a message and a description", async () => {
+      const response = await request(api).get("/")
+
+      expect(response.statusCode).toBe(200)
+      expect(response).toBe("Server is Running")
+    })
+  })
+  
+})
