947edef56781b20692699f2885f34d1b6649eefb
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-16 10:44:57 +0000

    added frontend register form logic to communicate with backend

---
+++ frontend/register.js
@@ -0,0 +1,28 @@
+document.getElementById("register-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: form.get("username"),
+            password: form.get("password"),
+            name: form.get("name"),
+            role: form.get("role")
+        })
+    }
+
+    const response = await fetch("http://localhost:3000/users/register", options);
+    const data = await response.json();
+
+    if (response.status == 201) {
+        window.location.assign("index.html");
+    } else {
+        alert(data.error);
+    }
+})
