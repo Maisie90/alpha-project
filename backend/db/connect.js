b591340114ce6ca60d8bbdc08d7765f129c64d0c
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-17 09:05:27 +0000

    add logging for database connection initalisation

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,7 +1,12 @@
 require('dotenv').config()
 
+console.log("running connect.js to create db")
+
+console.log("DB_URL:", process.env.DB_URL || '<missing>');
+if (!process.env.DB_URL) throw new Error('DB_URL not provided in environment');
+
 const { Pool } = require('pg')
-const db = new Pool ({                        
+const db = new Pool ({       
     connectionString: process.env.DB_URL,     
 })
 module.exports = db
