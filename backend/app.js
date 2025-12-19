cde1ab7bd788f9c9cd92a9a3983c70f04e911909
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-19 00:47:22 +0000

    added logger middleware

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const userRouter = require('./routers/user');
-const quizRouter = require('./routers/quiz');
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const userRouter = require("./routers/user")
+const quizRouter = require("./routers/quiz")
+const logger = require("./middleware/logger");
+
+//Middleware to log requests
+app.use(logger);
 
 //Middleware to parese JSON Bodies (Handling data from frontend)
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
 //check if backend is working
-app.get('/',(req, res) => {
-    res.status(200).send('Server is Running');
+app.get("/", (req, res) => {
+  res.status(200).send("Server is Running")
+})
 
-});
+app.use("/users", userRouter)
+app.use("/quizzes", quizRouter)
 
-app.use("/users", userRouter);
-app.use("/quizzes", quizRouter);
-
-module.exports = app;
+module.exports = app
